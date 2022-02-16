509748c3256debd09c923b7ded4276bf0f302d78
Salbih Mustafa <salbihdmustafa@gmail.com>
2022-02-16 13:49:45 -0600

    creating readfileasync method

--- src/components/UploadForm/UploadForm.js
+++ src/components/UploadForm/UploadForm.js
@@ -1,24 +1,66 @@
 import "./UploadForm.css";
-import React from "react";
+import React, { useState } from "react";
 import Card from "../Card/Card";
 
 const UploadForm = () => {
-  const fileName1 = "Testing.csv";
-  const fileName2 = "Testing.csv";
+  const [input1, setInput1] = useState("");
+  const [input2, setInput2] = useState("");
+
+  const uploadFile1 = (event) => {
+    setInput1(event.target.files[0]);
+  };
+
+  const uploadFile2 = (event) => {
+    setInput2(event.target.files[0]);
+  };
+
+  const clearForm = () => {
+    setInput1("");
+    setInput2("");
+  };
+
+  const submitCompare = async (e) => {
+    e.preventDefault();
+
+    try {
+      let contentFile1 = await readFileAsync(input1);
+      debugger;
+      console.log(contentFile1);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const readFileAsync = (csvFile) => {
+    return new Promise((resolve, reject) => {
+        debugger;
+      let reader = new FileReader();
+
+      reader.onload = (res) => {
+        resolve(res.target.result); //return the value to the await
+      };
+      reader.onerror = (err) => console.log(err);
+      reader.readAsText(csvFile);
+    });
+  };
+
   return (
     <Card>
-      <form className="center-form">
-        <div className="label-space-right">
+      <form className="center-form" onSubmit={submitCompare}>
+        <div className="flex-side">
           <label>Upload CSV:</label>
-          <button>Upload</button>
-          <label>{fileName1}</label>
+          <input type="file" accept=".csv" onChange={uploadFile1} />
         </div>
-        <div className="label-space-right">
+        <div className="flex-side">
           <label>Upload CSV:</label>
-          <button>Upload</button>
-          <label>{fileName2}</label>
+          <input type="file" accept=".csv" onChange={uploadFile2} />
         </div>
-        <button>Compare</button>
+        <div className="center-buttons">
+          <button type="reset" onClick={clearForm}>
+            Clear
+          </button>
+          <button type="submit">Compare</button>
+        </div>
       </form>
     </Card>
   );
